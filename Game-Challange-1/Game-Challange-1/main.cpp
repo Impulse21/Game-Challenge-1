24c791ac1ec9b88e85c4bb93f101b824100ffef9
cdipaolo <christopher.dipaolo@incognito.com>
2016-11-22 13:07:07 -0500

    Reworked project layout to support CMake

---
+++ Game-Challange-1/Game-Challange-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game-Challange-1
+//
+//  Created by Christopher DiPaolo on 2016-11-22.
+//  Copyright © 2016 Christopher DiPaolo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
