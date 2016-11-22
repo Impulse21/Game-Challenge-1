a2174c3717deab72eaeaebf9af29aa71dd3e8628
impulse21 <dipaolo21@gmail.com>
2016-11-21 21:34:30 -0500

    Init Project Setup

---
+++ GameChallange-1.cpp
@@ -0,0 +1,42 @@
+// GameChallange-1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+#define MSG_INTRO			"Welcome to the war of humans vs zombies, here we will simulate the battle that will decide the outcome of the human race."
+#define MSG_HUMAN_INSERT	"Please enter the size of the human army:"
+#define MSG_ZOMBIE_INSERT	"Please enter the size of the zombie army:"
+#define MSG_STATS			"Number of zombies left: %i"
+
+struct STATS 
+{
+	int health;
+	int armour;
+};
+
+// Prototypes
+int getArmySizeInput(char * message);
+
+int main()
+{
+	std::cout << MSG_INTRO;
+
+	int hunamArmySize = getArmySizeInput(MSG_HUMAN_INSERT);
+	int zombieArmySize = getArmySizeInput(MSG_ZOMBIE_INSERT);
+
+	
+    return 0;
+}
+
+int getArmySizeInput(char * message)
+{
+	int armySize = 0;
+
+	std::cout << message;
+
+	std::cin >> armySize;
+
+	return armySize;
+}
+
