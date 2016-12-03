331c8d0a05233ca89d5b66f3dbd40b761a96bbb5
cdipaolo <christopher.dipaolo@incognito.com>
2016-12-03 00:24:08 -0500

    Updated logic

--- src/GameChallange-1.cpp
+++ src/GameChallange-1.cpp
@@ -1,10 +1,9 @@
 // GameChallange-1.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include <iostream>
-#include "Army.h"
-#include "GameManager.h"
+#include <stdlib.h>
+#include <time.h>
 
 #define MSG_INTRO			"Welcome to the war of humans vs zombies, here we will simulate the battle that will decide the outcome of the human race."
 #define MSG_HUMAN_INSERT	"Please enter the size of the human army:"
@@ -12,41 +11,89 @@
 #define MSG_STATS			"Number of zombies left: %i"
 
 
+struct ARMY_STRUCT
+{
+	int size;
+	int health;
+	int attack;
+};
 
 // Prototypes
 int getArmySizeInput(char * message);
+bool isGameOver(int const& armyOneSize, int const& armyTwoSize);
+void battle(ARMY_STRUCT &armyOne, ARMY_STRUCT &armyTwo);
 
 int main()
 {
 	std::cout << MSG_INTRO;
 
-	Army humanArmy("HumanArmy");
-	Army zombieArmy("ZombieArmy");
+	ARMY_STRUCT humanArmy;
+	humanArmy.health = 200;
+	humanArmy.attack = 100;
 
-	//humanArmy.setArmySize(getArmySizeInput(MSG_HUMAN_INSERT);
-	//zombieArmy.setArmySize(getArmySizeInput(MSG_ZOMBIE_INSERT);
+	ARMY_STRUCT zombieArmy;
+	zombieArmy.health = 400;
+	zombieArmy.attack = 50;
 
-	int humanArmySize = getArmySizeInput(MSG_HUMAN_INSERT);
-	int zombieArmySize = getArmySizeInput(MSG_HUMAN_INSERT);
+	humanArmy.size = getArmySizeInput(MSG_HUMAN_INSERT);
+	zombieArmy.size = getArmySizeInput(MSG_HUMAN_INSERT);
+
+	// Set the random seed.
+	srand(time(NULL));
 
 	bool gameOver = false;
 
 	while(!gameOver)
 	{
-		
+		battle(humanArmy, zombieArmy);
+
+		gameOver = isGameOver(humanArmy.size, zombieArmy.size);
 	}
 
-	GameManager game(humanArmy, zombieArmy);
+	if(humanArmy.size > 0)
+	{
+		std::cout << "The Humans won with " << humanArmy.size << " left";
+	}
+	else
+	{
+		std::cout << "Zombies won with " << zombieArmy.size << " left";
+	}
 
-	game.Run();
-
-	// Check who won the game.
     return 0;
 }
 
-bool first(int &armyOne, int &armyTwo)
+bool isGameOver(int const& armyOneSize, int const& armyTwoSize)
 {
-	// Random Gen 
+	return (armyOneSize <= 0 || armyTwoSize <= 0);
+}
+
+void battle(ARMY_STRUCT &armyOne, ARMY_STRUCT &armyTwo)
+{
+	int healthOne = armyOne.health;
+	int healthTwo = armyTwo.health;
+
+	while(healthOne > 0 || healthTwo > 0)
+	{
+		int diceRoll = rand() % 2 + 1;
+		
+		if(diceRoll == 2)
+		{
+			healthOne -= armyTwo.attack;
+		}
+		else
+		{
+			healthTwo -= armyOne.attack;
+		}
+	}
+	
+	if(healthOne < 0)
+	{
+		armyOne.size -= 1;
+	}
+	else
+	{
+		armyTwo.size -= 1;
+	}
 }
 
 int getArmySizeInput(char * message)
